e7baa7adefce54240ece4f48613a8f06c4975c7e
MaduraDiasTreinetic <102131928+MaduraDiasTreinetic@users.noreply.github.com>
2022-08-04 10:42:19 +0530

    Update vpn_engine.dart

--- lib/src/vpn_engine.dart
+++ lib/src/vpn_engine.dart
@@ -161,10 +161,10 @@ class OpenVPN {
           if (Platform.isIOS) {
             var splitted = value.split("_");
             var connectedOn = DateTime.tryParse(splitted[0]);
-            // if (connectedOn == null) return VpnStatus.empty();
+            if (connectedOn == null) return VpnStatus.empty();
             return VpnStatus(
               connectedOn: connectedOn,
-              //duration: _duration(DateTime.now().difference(connectedOn).abs()),
+              duration: _duration(DateTime.now().difference(connectedOn).abs()),
               packetsIn: splitted[1],
               packetsOut: splitted[2],
               byteIn: splitted[3],
@@ -181,10 +181,10 @@ class OpenVPN {
                 data["byte_out"] != null ? data["byte_out"].toString() : "0";
             if (byteIn.trim().isEmpty) byteIn = "0";
             if (byteOut.trim().isEmpty) byteOut = "0";
-            // if (connectedOn == null) return VpnStatus.empty();
+             if (connectedOn == null) return VpnStatus.empty();
             return VpnStatus(
               connectedOn: connectedOn,
-              // duration: _duration(DateTime.now().difference(connectedOn).abs()),
+              duration: _duration(DateTime.now().difference(connectedOn).abs()),
               byteIn: byteIn,
               byteOut: byteOut,
               packetsIn: byteIn,
